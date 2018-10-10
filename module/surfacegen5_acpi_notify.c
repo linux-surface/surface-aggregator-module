854fae13cddc636b77481f6cb6f9b2388a36f0e2
qzed <qzed@users.noreply.github.com>
2018-10-10 18:15:53 +0200

    Implement input validation

--- module/surfacegen5_acpi_notify.c
+++ module/surfacegen5_acpi_notify.c
@@ -18,70 +18,104 @@ struct gsb_data_in {
 } __packed;
 
 struct gsb_data_rqsx {
-	u8 cv;
-	u8 tc;
-	u8 tid;
-	u8 iid;
-	u8 snc;
-	u8 cid;
-	u8 cdl;
-	u8 _pad;
-	u8 pld[0];
+	u8 cv;				// command value (should be 0x01 or 0x03)
+	u8 tc;				// target controller
+	u8 tid;				// expected to be 0x01, could be revision
+	u8 iid;				// target sub-controller (e.g. primary vs. secondary battery)
+	u8 snc;				// expect-response-flag
+	u8 cid;				// command ID
+	u8 cdl;				// payload length
+	u8 _pad;			// padding
+	u8 pld[0];			// payload
 } __packed;
 
 struct gsb_data_etwl {
-	u8 cv;
-	u8 etw3;
-	u8 etw4;
-	char msg[0];
+	u8 cv;				// command value (should be 0x02)
+	u8 etw3;			// ?
+	u8 etw4;			// ?
+	u8 msg[0];			// error message (ASCIIZ)
 } __packed;
 
 struct gsb_data_out {
-	u8 status;
-	u8 len;
-	u8 pld[0];
+	u8 status;			// _SSH communication status
+	u8 len;				// _SSH payload length
+	u8 pld[0];			// _SSH payload
 } __packed;
 
 union gsb_buffer_data {
-	struct gsb_data_in   in;    // common input
-	struct gsb_data_rqsx rqsx;  // RQSX input
-	struct gsb_data_etwl etwl;  // ETWL input
-	struct gsb_data_out  out;   // output
+	struct gsb_data_in   in;	// common input
+	struct gsb_data_rqsx rqsx;	// RQSX input
+	struct gsb_data_etwl etwl;	// ETWL input
+	struct gsb_data_out  out;	// output
 };
 
 struct gsb_buffer {
-	u8 status;
-	u8 len;
+	u8 status;			// GSB AttribRawProcess status
+	u8 len;				// GSB AttribRawProcess length
 	union gsb_buffer_data data;
 } __packed;
 
+
+static struct gsb_data_rqsx *surfacegen5_validate_rqsx(
+	struct device *dev, const char *type, struct gsb_buffer *buffer)
+{
+	struct gsb_data_rqsx *rqsx = &buffer->data.rqsx;
+
+	if (buffer->len < 8) {
+		dev_err(dev, "invalid %s package (len = %d)\n",
+			type, buffer->len);
+		return NULL;
+	}
+
+	if (rqsx->cdl != buffer->len - 8) {
+		dev_err(dev, "bogus %s package (len = %d, cdl = %d)\n",
+			type, buffer->len, rqsx->cdl);
+		return NULL;
+	}
+
+	if (rqsx->tid != 0x01) {
+		dev_warn(dev, "unsupported %s package (tid = 0x%02x)\n",
+			 type, rqsx->tid);
+		return NULL;
+	}
+
+	return rqsx;
+}
 
 static acpi_status
 surfacegen5_etwl(struct surfacegen5_handler_context *ctx, struct gsb_buffer *buffer)
 {
 	struct gsb_data_etwl *etwl = &buffer->data.etwl;
 
-	u8 msglen = buffer->len - 3;
-	u8 etw3   = etwl->etw3;
-	u8 etw4   = etwl->etw4;
-	char *msg = etwl->msg;
+	if (buffer->len < 3) {
+		dev_err(ctx->dev, "invalid ETWL package (len = %d)\n", buffer->len);
+		return AE_OK;
+	}
 
-	dev_err(ctx->dev, "ETWL(0x%02x, 0x%02x): %.*s\n", etw3, etw4, msglen, msg);
+	dev_err(ctx->dev, "ETWL(0x%02x, 0x%02x): %.*s\n",
+		etwl->etw3, etwl->etw4,
+		buffer->len - 3, (char *)etwl->msg);
+
+	// indicate success
+	buffer->status = 0x00;
+	buffer->len = 0x00;
+
 	return AE_OK;
 }
 
 static acpi_status
 surfacegen5_rqst(struct surfacegen5_handler_context *ctx, struct gsb_buffer *buffer)
 {
-	struct gsb_data_rqsx *rqsx = &buffer->data.rqsx;
+	struct gsb_data_rqsx *rqst = surfacegen5_validate_rqsx(ctx->dev, "RQST", buffer);
+	if (!rqst) { return AE_OK; }
 
-	// temporary fix for base status (lid notify loop)
+	// FIXME: temporary fix for base status (lid notify loop)
 	if (
-		rqsx->tc  == 0x11 &&
-		rqsx->tid == 0x01 &&
-		rqsx->iid == 0x00 &&
-		rqsx->snc == 0x01 &&
-		rqsx->cid == 0x0D
+		rqst->tc  == 0x11 &&
+		rqst->tid == 0x01 &&
+		rqst->iid == 0x00 &&
+		rqst->snc == 0x01 &&
+		rqst->cid == 0x0D
 	) {
 		buffer->status          = 0x00;
 		buffer->len             = 0x03;
@@ -92,18 +126,25 @@ surfacegen5_rqst(struct surfacegen5_handler_context *ctx, struct gsb_buffer *buf
 		return AE_OK;
 	}
 
-	// TODO:
+	// TODO: implement RQST handler
 
-	dev_warn(ctx->dev, "unsupported RQST request (tc: 0x%02x, cid: 0x%02x)\n", rqsx->tc, rqsx->cid);
+	dev_warn(ctx->dev, "unsupported request: RQST(0x%02x, 0x%02x, 0x%02x)\n",
+			 rqst->tc, rqst->cid, rqst->iid);
+
 	return AE_OK;
 }
 
 static acpi_status
 surfacegen5_rqsg(struct surfacegen5_handler_context *ctx, struct gsb_buffer *buffer)
 {
+	struct gsb_data_rqsx *rqsg = surfacegen5_validate_rqsx(ctx->dev, "RQSG", buffer);
+	if (!rqsg) { return AE_OK; }
+
 	// TODO: implement RQSG handler
 
-	dev_warn(ctx->dev, "unsupported RQSG request\n");
+	dev_warn(ctx->dev, "unsupported request: RQSG(0x%02x, 0x%02x, 0x%02x)\n",
+		 rqsg->tc, rqsg->cid, rqsg->iid);
+
 	return AE_OK;
 }
 
@@ -115,11 +156,23 @@ surfacegen5_space_handler(u32 function, acpi_physical_address command,
 {
 	struct surfacegen5_handler_context *context = handler_context;
 	struct gsb_buffer *buffer = (struct gsb_buffer *)value64;
+	int accessor_type = (0xFFFF0000 & function) >> 16;
 
-	// TODO: input validation
-	// - check lenght of buffer (min. 2 and consistency)
-	// - check if command == SAN0 (== 0) ?
-	// - check if AttribRawProcessCall?
+	if (command != 0) {
+		dev_warn(context->dev, "unsupported command: 0x%02llx\n", command);
+		return AE_OK;
+	}
+
+	if (accessor_type != ACPI_GSB_ACCESS_ATTRIB_RAW_PROCESS) {
+		dev_err(context->dev, "invalid access type: 0x%02x\n", accessor_type);
+		return AE_OK;
+	}
+
+	// buffer must have at least contain the command-value
+	if (buffer->len == 0) {
+		dev_err(context->dev, "request-package too small\n");
+		return AE_OK;
+	}
 
 	switch (buffer->data.in.cv) {
 	case 0x01:
@@ -130,7 +183,7 @@ surfacegen5_space_handler(u32 function, acpi_physical_address command,
 		return surfacegen5_rqsg(context, buffer);
 	}
 
-	dev_dbg(context->dev, "unsupported request (cv: 0x%02x)\n", buffer->data.in.cv);
+	dev_warn(context->dev, "unsupported SAN0 request (cv: 0x%02x)\n", buffer->data.in.cv);
 	return AE_OK;
 }
 
