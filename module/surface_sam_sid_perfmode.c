2ffd380b2b83eb38680c012b286e1bb1be5c2456
Maximilian Luz <luzmaximilian@gmail.com>
2019-11-12 20:14:33 +0100

    Fix whitespace issues

--- module/surface_sam_sid_perfmode.c
+++ module/surface_sam_sid_perfmode.c
@@ -154,7 +154,7 @@ static ssize_t perf_mode_store(struct device *dev, struct device_attribute *attr
 	//       which calls
 	//           ODV3 = ToInteger (Arg3)
 	//           Notify(IETM, 0x88)
- 	//       IETM is an INT3400 Intel Dynamic Power Performance Management
+	//       IETM is an INT3400 Intel Dynamic Power Performance Management
 	//       device, part of the DPTF framework. From the corresponding
 	//       kernel driver, it looks like event 0x88 is being ignored. Also
 	//       it is currently unknown what the consequecnes of setting ODV3
