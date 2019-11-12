d79f8e2002b2456ad78dceddf6d34b3ec6f60dde
Maximilian Luz <luzmaximilian@gmail.com>
2019-11-12 15:36:40 +0100

    scripts: Update unload.sh script

--- scripts/unload.sh
+++ scripts/unload.sh
@@ -5,8 +5,11 @@
 
 sudo systemctl stop surface-dtx-daemon.service
 
-sudo modprobe -r surface_sam_dtx
+sudo modprobe -r surface_sam_sid_vhf
+sudo modprobe -r surface_sam_sid_perfmode
+sudo modprobe -r surface_sam_sid_gpelid
 sudo modprobe -r surface_sam_sid
+sudo modprobe -r surface_sam_dtx
 sudo modprobe -r surface_sam_vhf
 sudo modprobe -r surface_sam_san
 sudo modprobe -r surface_sam_ssh
