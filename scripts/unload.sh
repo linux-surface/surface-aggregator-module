055c6a3e29195200beaec72c9040d4c8f78b5386
Maximilian Luz <luzmaximilian@gmail.com>
2020-01-04 19:46:41 +0100

    scripts: Add HPS module to unload script

--- scripts/unload.sh
+++ scripts/unload.sh
@@ -9,6 +9,7 @@ sudo modprobe -r surface_sam_sid_vhf
 sudo modprobe -r surface_sam_sid_perfmode
 sudo modprobe -r surface_sam_sid_gpelid
 sudo modprobe -r surface_sam_sid
+sudo modprobe -r surface_sam_hps
 sudo modprobe -r surface_sam_dtx
 sudo modprobe -r surface_sam_vhf
 sudo modprobe -r surface_sam_san
