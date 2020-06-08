81fb2da2d28c9a4702f03eefdada11d86bf21f7d
Maximilian Luz <luzmaximilian@gmail.com>
2020-06-08 22:13:17 +0200

    Make SB3 test script more generic

--- scripts/sb3_test.py
+++ scripts/sb3_test.py
@@ -5,6 +5,14 @@ import os
 PATH_DEV_RQST = '/sys/bus/serial/devices/serial0-0/rqst'
 
 
+EVCMDS = {
+    'enable':         {'tc': 0x21, 'cid': 0x01, 'iid': 0x00, 'snc': 0x01},
+    'disable':        {'tc': 0x21, 'cid': 0x02, 'iid': 0x00, 'snc': 0x01},
+    'legacy-enable':  {'tc': 0x01, 'cid': 0x0b, 'iid': 0x00, 'snc': 0x01},
+    'legacy-disable': {'tc': 0x01, 'cid': 0x0c, 'iid': 0x00, 'snc': 0x01},
+}
+
+
 def lo16(rqid):
     return rqid & 0xff
 
@@ -13,51 +21,20 @@ def hi16(rqid):
     return (rqid >> 8) & 0xff
 
 
-def enable_event(rqid, tc, iid, seq):
-    #            [  TC,  CID,  IID,  PRI,  SNC,  CDL, payload...]
-    return bytes([0x21, 0x01, 0x00, 0x02, 0x01, 0x05, tc, seq, lo16(rqid), hi16(rqid), iid])
+def event_payload(rqid, tc, iid, seq):
+    return bytes([tc, seq, lo16(rqid), hi16(rqid), iid])
 
 
-def disable_event(rqid, tc, iid, seq):
-    #            [  TC,  CID,  IID,  PRI,  SNC,  CDL, payload...]
-    return bytes([0x21, 0x02, 0x00, 0x02, 0x01, 0x05, tc, seq, lo16(rqid), hi16(rqid), iid])
+def command(tc, cid, iid, chn, snc, payload):
+    return bytes([tc, cid, iid, chn, snc, len(payload)]) + payload
 
 
-def sys_event_enable(rqid, tc, pri):
-    #            [  TC,  CID,  IID,  PRI,  SNC,  CDL, payload...]
-    return bytes([0x01, 0x0b, 0x00, 0x01, 0x01, 0x04, tc, pri, lo16(rqid), hi16(rqid)])
+def event_command(name, chn, ev_tc, ev_seq, ev_iid):
+    payload = event_payload(ev_tc, ev_tc, ev_iid, ev_seq)
+    return command(**EVCMDS[name], chn=chn, payload=payload)
 
 
-def sys_event_disable(rqid, tc, pri):
-    #            [  TC,  CID,  IID,  PRI,  SNC,  CDL, payload...]
-    return bytes([0x01, 0x0c, 0x00, 0x01, 0x01, 0x04, tc, pri, lo16(rqid), hi16(rqid)])
-
-
-def main():
-    if len(sys.argv) != 2:
-        print("not a valid command: choose one of:")
-        print("    setup, teardown, enable, disable")
-        exit(1)
-
-    cmd = sys.argv[1]
-    if cmd == "setup":
-        data = enable_event(0x21, 0x21, 0x00, 0x01)
-    elif cmd == "teardown":
-        data = disable_event(0x21, 0x21, 0x00, 0x01)
-    elif cmd == "enable":
-        data = enable_event(0x15, 0x15, 0x01, 0x00)
-    elif cmd == "disable":
-        data = disable_event(0x15, 0x15, 0x01, 0x00)
-    elif cmd == "kbd-enable":
-        data = sys_event_enable(0x15, 0x15, int(sys.argv[2], 0))
-    elif cmd == "kbd-disable":
-        data = sys_event_disable(0x15, 0x15, int(sys.argv[2], 0))
-        pass
-    else:
-        print("not a valid command: choose one of:")
-        print("    setup, teardown, enable, disable")
-        exit(1)
-
+def run_command(data):
     fd = os.open(PATH_DEV_RQST, os.O_RDWR | os.O_SYNC)
     os.write(fd, data)
 
@@ -68,6 +45,16 @@ def main():
     os.close(fd)
 
     print(' '.join(['{:02x}'.format(x) for x in data]))
+
+
+def main():
+    cmd_name = sys.argv[1]
+    channel = int(sys.argv[2], 0)
+    ev_tc = int(sys.argv[3], 0)
+    ev_seq = int(sys.argv[4], 0)
+    ev_iid = int(sys.argv[5], 0)
+
+    run_command(event_command(cmd_name, channel, ev_tc, ev_seq, ev_iid))
 
 
 if __name__ == '__main__':
