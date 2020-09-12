fcd6a1c7d3739e3af63e41c9479c04473c75aa99
Maximilian Luz <luzmaximilian@gmail.com>
2020-09-12 23:56:16 +0200

    dbgfs: Rename functions

--- module/src/clients/surface_sam_debugfs.c
+++ module/src/clients/surface_sam_debugfs.c
@@ -10,12 +10,11 @@
 
 #include "../../include/linux/surface_aggregator_module.h"
 
-#define SSAM_DBGDEV_NAME	"surface_sam_dbgdev"
-#define SSAM_DBGDEV_VERS	0x010000
-
+#define SSAM_DBG_DEVICE_NAME		"ssam_debug"
+#define SSAM_DBG_IF_VERSION		0x010000
 
 /**
- * struct ssam_dbgdev_request - Controller request IOCTL argument.
+ * struct ssam_debug_request - Controller request IOCTL argument.
  * @target_category: Target category of the SAM request.
  * @target_id:       Target ID of the SAM request.
  * @command_id:      Command ID of the SAM request.
@@ -31,7 +30,7 @@
  *                   On output: Length of request response (number of bytes
  *                   in the buffer that are actually used).
  */
-struct ssam_dbgdev_request {
+struct ssam_dbg_request {
 	__u8 target_category;
 	__u8 target_id;
 	__u8 command_id;
@@ -52,33 +51,31 @@ struct ssam_dbgdev_request {
 	} response;
 };
 
-#define SSAM_DBGDEV_IOCTL_GETVERSION  _IOR(0xA5, 0, __u32)
-#define SSAM_DBGDEV_IOCTL_REQUEST     _IOWR(0xA5, 1, struct ssam_dbgdev_request)
+#define SSAM_DBG_IOCTL_GETVERSION  _IOR(0xA5, 0, __u32)
+#define SSAM_DBG_IOCTL_REQUEST     _IOWR(0xA5, 1, struct ssam_dbg_request)
 
-
-struct ssam_dbgdev {
+struct ssam_dbg_data {
 	struct ssam_controller *ctrl;
 	struct dentry *dentry_dir;
 	struct dentry *dentry_dev;
 };
 
-
-static int ssam_dbgdev_open(struct inode *inode, struct file *filp)
+static int ssam_dbg_device_open(struct inode *inode, struct file *filp)
 {
 	filp->private_data = inode->i_private;
 	return nonseekable_open(inode, filp);
 }
 
-static long ssam_dbgdev_request(struct file *file, unsigned long arg)
+static long ssam_dbg_if_request(struct file *file, unsigned long arg)
 {
-	struct ssam_dbgdev *ddev = file->private_data;
-	struct ssam_dbgdev_request __user *r;
-	struct ssam_dbgdev_request rqst;
+	struct ssam_dbg_data *data = file->private_data;
+	struct ssam_dbg_request __user *r;
+	struct ssam_dbg_request rqst;
 	struct ssam_request spec;
 	struct ssam_response rsp;
 	int status = 0, ret = 0, tmp;
 
-	r = (struct ssam_dbgdev_request __user *)arg;
+	r = (struct ssam_dbg_request __user *)arg;
 	ret = copy_struct_from_user(&rqst, sizeof(rqst), r, sizeof(*r));
 	if (ret)
 		goto out;
@@ -133,7 +130,7 @@ static long ssam_dbgdev_request(struct file *file, unsigned long arg)
 	}
 
 	// perform request
-	status = ssam_request_sync(ddev->ctrl, &spec, &rsp);
+	status = ssam_request_sync(data->ctrl, &spec, &rsp);
 	if (status)
 		goto out;
 
@@ -162,38 +159,38 @@ out:
 	return ret;
 }
 
-static long ssam_dbgdev_getversion(struct file *file, unsigned long arg)
+static long ssam_dbg_if_getversion(struct file *file, unsigned long arg)
 {
-	put_user(SSAM_DBGDEV_VERS, (u32 __user *)arg);
+	put_user(SSAM_DBG_IF_VERSION, (u32 __user *)arg);
 	return 0;
 }
 
-static long ssam_dbgdev_ioctl(struct file *file, unsigned int cmd,
-			      unsigned long arg)
+static long ssam_dbg_device_ioctl(struct file *file, unsigned int cmd,
+				    unsigned long arg)
 {
 	switch (cmd) {
-	case SSAM_DBGDEV_IOCTL_GETVERSION:
-		return ssam_dbgdev_getversion(file, arg);
+	case SSAM_DBG_IOCTL_GETVERSION:
+		return ssam_dbg_if_getversion(file, arg);
 
-	case SSAM_DBGDEV_IOCTL_REQUEST:
-		return ssam_dbgdev_request(file, arg);
+	case SSAM_DBG_IOCTL_REQUEST:
+		return ssam_dbg_if_request(file, arg);
 
 	default:
 		return -ENOIOCTLCMD;
 	}
 }
 
-const struct file_operations ssam_dbgdev_fops = {
+const struct file_operations ssam_dbg_device_fops = {
 	.owner          = THIS_MODULE,
-	.open           = ssam_dbgdev_open,
-	.unlocked_ioctl = ssam_dbgdev_ioctl,
-	.compat_ioctl   = ssam_dbgdev_ioctl,
+	.open           = ssam_dbg_device_open,
+	.unlocked_ioctl = ssam_dbg_device_ioctl,
+	.compat_ioctl   = ssam_dbg_device_ioctl,
 	.llseek         = noop_llseek,
 };
 
-static int ssam_dbgdev_probe(struct platform_device *pdev)
+static int ssam_dbg_device_probe(struct platform_device *pdev)
 {
-	struct ssam_dbgdev *ddev;
+	struct ssam_dbg_data *data;
 	struct ssam_controller *ctrl;
 	int status;
 
@@ -201,81 +198,80 @@ static int ssam_dbgdev_probe(struct platform_device *pdev)
 	if (status)
 		return status == -ENXIO ? -EPROBE_DEFER : status;
 
-	ddev = devm_kzalloc(&pdev->dev, sizeof(struct ssam_dbgdev), GFP_KERNEL);
-	if (!ddev)
+	data = devm_kzalloc(&pdev->dev, sizeof(struct ssam_dbg_data), GFP_KERNEL);
+	if (!data)
 		return -ENOMEM;
 
-	ddev->ctrl = ctrl;
+	data->ctrl = ctrl;
 
-	ddev->dentry_dir = debugfs_create_dir("ssam", NULL);
-	if (IS_ERR(ddev->dentry_dir))
-		return PTR_ERR(ddev->dentry_dir);
+	data->dentry_dir = debugfs_create_dir("ssam", NULL);
+	if (IS_ERR(data->dentry_dir))
+		return PTR_ERR(data->dentry_dir);
 
-	ddev->dentry_dev = debugfs_create_file("controller", 0600,
-					       ddev->dentry_dir, ddev,
-					       &ssam_dbgdev_fops);
-	if (IS_ERR(ddev->dentry_dev)) {
-		debugfs_remove(ddev->dentry_dir);
-		return PTR_ERR(ddev->dentry_dev);
+	data->dentry_dev = debugfs_create_file("controller", 0600,
+					       data->dentry_dir, data,
+					       &ssam_dbg_device_fops);
+	if (IS_ERR(data->dentry_dev)) {
+		debugfs_remove(data->dentry_dir);
+		return PTR_ERR(data->dentry_dev);
 	}
 
-	platform_set_drvdata(pdev, ddev);
+	platform_set_drvdata(pdev, data);
 	return 0;
 }
 
-static int ssam_dbgdev_remove(struct platform_device *pdev)
+static int ssam_dbg_device_remove(struct platform_device *pdev)
 {
-	struct ssam_dbgdev *ddev = platform_get_drvdata(pdev);
+	struct ssam_dbg_data *data = platform_get_drvdata(pdev);
 
-	debugfs_remove(ddev->dentry_dev);
-	debugfs_remove(ddev->dentry_dir);
+	debugfs_remove(data->dentry_dev);
+	debugfs_remove(data->dentry_dir);
 
 	platform_set_drvdata(pdev, NULL);
 	return 0;
 }
 
-static void ssam_dbgdev_release(struct device *dev)
+static void ssam_dbg_device_release(struct device *dev)
 {
 	// nothing to do
 }
 
-
-static struct platform_device ssam_dbgdev_device = {
-	.name = SSAM_DBGDEV_NAME,
+static struct platform_device ssam_dbg_device = {
+	.name = SSAM_DBG_DEVICE_NAME,
 	.id = PLATFORM_DEVID_NONE,
-	.dev.release = ssam_dbgdev_release,
+	.dev.release = ssam_dbg_device_release,
 };
 
-static struct platform_driver ssam_dbgdev_driver = {
-	.probe = ssam_dbgdev_probe,
-	.remove = ssam_dbgdev_remove,
+static struct platform_driver ssam_dbg_driver = {
+	.probe = ssam_dbg_device_probe,
+	.remove = ssam_dbg_device_remove,
 	.driver = {
-		.name = SSAM_DBGDEV_NAME,
+		.name = SSAM_DBG_DEVICE_NAME,
 	},
 };
 
-static int __init surface_sam_debugfs_init(void)
+static int __init ssam_debug_init(void)
 {
 	int status;
 
-	status = platform_device_register(&ssam_dbgdev_device);
+	status = platform_device_register(&ssam_dbg_device);
 	if (status)
 		return status;
 
-	status = platform_driver_register(&ssam_dbgdev_driver);
+	status = platform_driver_register(&ssam_dbg_driver);
 	if (status)
-		platform_device_unregister(&ssam_dbgdev_device);
+		platform_device_unregister(&ssam_dbg_device);
 
 	return status;
 }
-module_init(surface_sam_debugfs_init);
+module_init(ssam_debug_init);
 
-static void __exit surface_sam_debugfs_exit(void)
+static void __exit ssam_debug_exit(void)
 {
-	platform_driver_unregister(&ssam_dbgdev_driver);
-	platform_device_unregister(&ssam_dbgdev_device);
+	platform_driver_unregister(&ssam_dbg_driver);
+	platform_device_unregister(&ssam_dbg_device);
 }
-module_exit(surface_sam_debugfs_exit);
+module_exit(ssam_debug_exit);
 
 MODULE_AUTHOR("Maximilian Luz <luzmaximilian@gmail.com>");
 MODULE_DESCRIPTION("DebugFS entries for Surface Aggregator Module");
