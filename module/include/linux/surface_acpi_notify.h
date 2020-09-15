0839b5e5596bc55d0b298c161da496faf0ea0e72
Maximilian Luz <luzmaximilian@gmail.com>
2020-09-15 14:27:31 +0200

    Fix comments

--- module/include/linux/surface_acpi_notify.h
+++ module/include/linux/surface_acpi_notify.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 /*
- * Interface for Surface ACPI/Notify (SAN) driver.
+ * Interface for Surface ACPI Notify (SAN) driver.
  *
  * Provides access to discrete GPU notifications sent from ACPI via the SAN
  * driver, which are not handled by this driver directly.
