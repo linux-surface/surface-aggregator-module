14442cf06fb522b710167bb845ce06495309eaaf
Maximilian Luz <luzmaximilian@gmail.com>
2021-02-14 17:20:24 +0100

    battery: Add missing includes

--- module/src/clients/surface_battery.c
+++ module/src/clients/surface_battery.c
@@ -12,7 +12,9 @@
 #include <linux/jiffies.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/mutex.h>
 #include <linux/power_supply.h>
+#include <linux/sysfs.h>
 #include <linux/types.h>
 #include <linux/workqueue.h>
 
